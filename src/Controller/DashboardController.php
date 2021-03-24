4ca8b20b0ac1fdee1083a6160a6d4f35369b09c8
jtrivino <jtrivino@webimpacto.es>
2021-03-24 20:08:37 +0100

    second commit

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -24,42 +24,8 @@ class DashboardController extends AbstractController
      */
     public function index(): JsonResponse
     {
-        $exportManager = new ExportManager();
-        $exportConfig = new ExportConfig();
-        $config = (object)[
-            "type" => "column2d",
-            "renderAt" => "chart-container",
-            "width" => "550",
-            "height" => "350",
-            "id" => "myChartId",
-            "dataFormat" => "json",
-            "dataSource" => (object)[
-                "chart" => (object)[
-                    "caption" => "Number of visitors last week",
-                    "theme" => "ocean",
-                    "subCaption" => "Bakersfield Central vs Los Angeles Topanga"
-                ],
-                "data" => [
-                    (object)[
-                        "label" => "Mon",
-                        "value" => "15123"
-                    ],
-                    (object)[
-                        "label" => "Tue",
-                        "value" => "14233"
-                    ],
-                    (object)[
-                        "label" => "Wed",
-                        "value" => "25507"
-                    ]
-                ]
-            ]
-        ];
-        $exportConfig->set('chartConfig', $config);
-        $files = $exportManager->export($exportConfig, '.', true);
-        var_dump($files);
-        die();
-       return new JsonResponse($schema, 200);
+
+       return new JsonResponse('ok', 200);
 //        return $this->render('dashboard/index.html.twig', [
 //            'controller_name' => 'DashboardController',
 //        ]);
